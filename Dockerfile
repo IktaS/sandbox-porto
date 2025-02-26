535209e757abe8c26d527900d792f22186d5520d
Imam Rafii <imamrafiia@gmail.com>
2025-02-26 18:56:16 +0700

    oh-my-bash

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ RUN apk update
 RUN apk upgrade
 RUN apk add fastfetch
 RUN apk add bash
+RUN apk add git
 ADD ./scripts /scripts
 RUN chmod +x /scripts/setup_user.sh
 RUN /scripts/setup_user.sh
--- scripts/setup_user.sh
+++ scripts/setup_user.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 adduser -D visitor
-cp /scripts/.bashrc /home/visitor/.bashrc
 mkdir -p /home/visitor/.config/fastfetch/
 cp /scripts/config.jsonc /home/visitor/.config/fastfetch/config.jsonc
+su visitor -c 'bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)"'
+cp /scripts/.bashrc /home/visitor/.bashrc
 chmod -R 555 /home/visitor
+touch /home/visitor/.bash_history
+chmod 777 /home/visitor/.bash_history
