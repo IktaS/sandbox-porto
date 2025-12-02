108576cd2a2524c3e6e61b209ca9f27377c43333
IktaS <imamrafiia@gmail.com>
2025-12-02 21:25:35 +0700

    update client color

--- src/entry-client.js
+++ src/entry-client.js
@@ -1,30 +1,33 @@
-import '@xterm/xterm/css/xterm.css';
-import { Terminal } from '@xterm/xterm';
-import { FitAddon } from '@xterm/addon-fit';
-import { ImageAddon, IImageAddonOptions } from '@xterm/addon-image';
-import { WebLinksAddon } from '@xterm/addon-web-links';
-import { Unicode11Addon } from '@xterm/addon-unicode11';
+import "@xterm/xterm/css/xterm.css";
+import { Terminal } from "@xterm/xterm";
+import { FitAddon } from "@xterm/addon-fit";
+import { ImageAddon, IImageAddonOptions } from "@xterm/addon-image";
+import { WebLinksAddon } from "@xterm/addon-web-links";
+import { Unicode11Addon } from "@xterm/addon-unicode11";
 
 const customSettings = {
-    enableSizeReports: true,    // whether to enable CSI t reports (see below)
-    pixelLimit: 16777216,       // max. pixel size of a single image
-    sixelSupport: true,         // enable sixel support
-    sixelScrolling: true,       // whether to scroll on image output
-    sixelPaletteLimit: 256,     // initial sixel palette size
-    sixelSizeLimit: 25000000,   // size limit of a single sixel sequence
-    storageLimit: 128,          // FIFO storage limit in MB
-    showPlaceholder: true,      // whether to show a placeholder for evicted images
-    iipSupport: true,           // enable iTerm IIP support
-    iipSizeLimit: 20000000      // size limit of a single IIP sequence
-  }
+  enableSizeReports: true, // whether to enable CSI t reports (see below)
+  pixelLimit: 16777216, // max. pixel size of a single image
+  sixelSupport: true, // enable sixel support
+  sixelScrolling: true, // whether to scroll on image output
+  sixelPaletteLimit: 256, // initial sixel palette size
+  sixelSizeLimit: 25000000, // size limit of a single sixel sequence
+  storageLimit: 128, // FIFO storage limit in MB
+  showPlaceholder: true, // whether to show a placeholder for evicted images
+  iipSupport: true, // enable iTerm IIP support
+  iipSizeLimit: 20000000, // size limit of a single IIP sequence
+};
 
 const socket = io({
-    extraHeaders: {
-      "mobile": document.documentElement.clientWidth < 900 ? true : false
-    }
-  });
+  extraHeaders: {
+    mobile: document.documentElement.clientWidth < 900 ? true : false,
+  },
+});
 var term = new Terminal({
-  allowProposedApi: true
+  allowProposedApi: true,
+  theme: {
+    background: "#708090",
+  },
 });
 const fit = new FitAddon();
 const image = new ImageAddon(customSettings);
@@ -34,23 +37,23 @@ term.loadAddon(fit);
 term.loadAddon(image);
 term.loadAddon(weblink);
 term.loadAddon(unicode);
-term.unicode.activeVersion = '11';
-const terminalContainer = document.getElementById('terminal');
+term.unicode.activeVersion = "11";
+const terminalContainer = document.getElementById("terminal");
 term.open(terminalContainer);
 term.onResize((data) => {
-    socket.emit("terminal.resize", data);
-})
+  socket.emit("terminal.resize", data);
+});
 fit.fit();
 
 socket.on("terminal.incomingData", (data) => {
-    term.write(data);
+  term.write(data);
 });
 
 term.onData((data) => {
-    socket.emit("terminal.keystroke", data);
+  socket.emit("terminal.keystroke", data);
 });
 
-const resizeObserver = new ResizeObserver(entries => {
-    fit.fit();
+const resizeObserver = new ResizeObserver((entries) => {
+  fit.fit();
 });
 resizeObserver.observe(terminalContainer);
